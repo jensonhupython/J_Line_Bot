f1bcf0b7c703488dce96715e930ddc427ebacd78
Jenson Hu <jenson7899@gmail.com>
2021-07-04 21:33:41 +0800

    [Python][Line-Bot] Fix Line-Bot reply message ver1

--- Line_Bot_app.py
+++ Line_Bot_app.py
@@ -13,8 +13,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi('Q+ACqQXK7JI1MGp8J4K2jKoj71irtyhxO9W4vR/vrdY9NewL4/0Z353Yi427gPUNXFX9i+mBSU6AgOqP72I5yO2heylmFmlVAzKf4aSvZ9Gjmx9lzsHV+yXg9i4pRRBZRlraG/dTgeGwjbMoFxgfPwdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('7f6587e82ce905e441ff65fe981299a2')
 
 
 @app.route("/callback", methods=['POST'])
@@ -38,10 +38,17 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    reply_msg = '看不懂你說什麼'
+
+    if msg == 'hi':
+        reply_msg = 'hi, how are you ?'
+    elif msg == '你吃飯了嗎':
+        reply_msg = '還沒'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
-
+        #TextSendMessage(text=event.message.text))
+        TextSendMessage(text=reply_msg))
 
 if __name__ == "__main__":
     app.run()
