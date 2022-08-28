d964a3d6bdff5a34a31d9ec65534da8ce8dbe616
Jenson Hu <jenson7899@gmail.com>
2022-08-28 23:36:54 +0800

    [Python][Line-bot] remove msg.encode utf-8

--- Line_Bot_app.py
+++ Line_Bot_app.py
@@ -39,7 +39,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    msg = msg.encode('utf-8')
+    #msg = msg.encode('utf-8')
 
     reply_msg = '看不懂你說什麼'
 
