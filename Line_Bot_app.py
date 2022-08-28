a540d21d3094b0018d28871bcfe70cd46eed8dba
Jenson Hu <jenson7899@gmail.com>
2022-08-28 13:34:13 +0800

    [Python][Line-bot] Improve HBD reply msg add emoji

--- Line_Bot_app.py
+++ Line_Bot_app.py
@@ -39,6 +39,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
+    msg = msg.encode('utf-8')
+
     reply_msg = '看不懂你說什麼'
 
     # Add reply 貼圖, do not forget import StickerSendMessage from linebot.model
@@ -187,8 +189,23 @@ def handle_message(event):
     elif '心情' in msg:
         reply_msg = '每天都要保持好的心情'
     elif '生日快樂' in msg:
-        reply_msg = '今天是您農曆八月二日誕生日,  祝您生日快樂, 萬事如意, 天天好心情~'
+        emoji = [
+        {
+            "index": 0, # 要放的位置
+            "productId": "5ac223c6040ab15980c9b44a", # 所屬組合 ID
+            "emojiId": "035" # emoji ID
+        },
+        {
+            "index": 37,
+            "productId": "5ac223c6040ab15980c9b44a",
+            "emojiId": "008"
+        }
+]
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='$ 今天是您農曆八月二日誕生日,  祝您生日快樂, 萬事如意, 天天好心情~ $'))
 
+        return 
     line_bot_api.reply_message(
         event.reply_token,
         # 目前修改程式碼的部分, 是調整讓機器人回覆什麼
